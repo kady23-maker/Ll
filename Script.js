b0865388bfc20acb0a7cb03cfe737f9ba7bd7d59
HD:Surya <134606888+kady23-maker@users.noreply.github.com>
2025-01-28 11:58:35 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+var whale = (function () {
+    var element = document.getElementById("whale"),
+        width = window.innerWidth || 
+                document.documentElement.clientWidth || 
+                document.body.clientWidth,
+        height = window.innerHeight || 
+                 document.documentElement.clientHeight || 
+                 document.body.clientHeight,
+        fps = 30, easy = 6,
+        maxspeed = 150,
+        delay = 15,
+        mouse = {x: width / 2, y: height / 2},
+        defs, parts;
+});
